43a96000a1c360faae9683d89515afb519ba4a60
lotfidev <lotfi.dev@gmail.com>
2015-12-21 16:08:48 +0100

    fdgf

--- www/js/localstorage.js
+++
@@ -1,6 +0,0 @@
-var db = openDatabase("my.db", '1.0', "My WebSQL Database", 2 * 1024 * 1024);
-var create = function() {
-    db.transaction(function (tx) {
-        tx.executeSql("CREATE TABLE IF NOT EXISTS user (id integer primary key, firstname text, lastname text, id_member text)");
-    });
-}
